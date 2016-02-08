4b54f40106a22ee53503f9e330ae2542b704082d
lbsonweb <lbs.on.web@gmail.com>
2016-02-08 20:09:01 -0200

    Create select.php

---
+++ select.php
@@ -0,0 +1,21 @@
+<?php
+  try {
+	require_once 'connect.php';
+	$conn=conexao();
+	
+     $stmt = $conn->prepare("SELECT id,nome, email FROM testandopdo"); 
+     $stmt->execute();
+
+     // set the resulting array to associative
+     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+
+     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+         echo $v;
+     }
+}
+catch(PDOException $e) {
+     echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+?>  
